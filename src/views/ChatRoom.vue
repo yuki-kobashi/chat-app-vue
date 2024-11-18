ee8cb9beebf1bf789c75751135acbc845349bc1b
yuki-kobashi <kd1344505@st.kobedenshi.ac.jp>
2024-11-18 14:26:45 +0900

    chat画面を実装

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="container">
     <NavigationBar />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
 import NavigationBar from '../components/NavigationBar.vue'
+import ChatWindow from '../components/ChatWindow'
+import axios from 'axios'
 
 export default {
-  components: { NavigationBar }
+  components: { NavigationBar,ChatWindow },
+  data () {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+  mounted() {
+    this.getMessages()
+  }
 }
 </script>
 
