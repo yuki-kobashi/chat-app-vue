5c204f7319370cdcc480ba14e1fa8477c9f8010b
yuki-kobashi <kd1344505@st.kobedenshi.ac.jp>
2024-11-18 10:27:35 +0900

    否認証時アクセスを制限する処理を実装

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,67 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import WelcomeView from '../views/WelcomeView'
 import Chatroom from '../views/ChatRoom'
+import useValidate from '../auth/validate'
 
 Vue.use(VueRouter)
+
+const { error, validate } = useValidate()
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'Welcome' })
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+
+  next()
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'WelcomeView',
-    component: WelcomeView
+    component: WelcomeView,
+    beforeEnter: noRequireAuth
   },
   
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter: requireAuth
   }
 ]
 
